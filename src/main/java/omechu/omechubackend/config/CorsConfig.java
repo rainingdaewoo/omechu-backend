2f9c4ea25f417a1bfbac685e239be53240bbf7de
김우중 <rainingdaewoo@gmail.com>
2022-08-03 17:23:41 +0900

    2022.07.27 - yml 개인정보 삭제

--- src/main/java/omechu/omechubackend/config/CorsConfig.java
+++ src/main/java/omechu/omechubackend/config/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig {
 
         //config.addAllowedOriginPattern("*");
 
-        config.addAllowedOrigin("http://localhost:3000");
+        //config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://master.d1y7zww8xijf8t.amplifyapp.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
